194de28ecc5d448dea97879a2573539ec80f57f0
UKhurshed <khurshedumarov777@gmail.com>
2023-06-06 10:22:25 +0500

    First commit

---
+++ NoteApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  NoteApp
+//
+//  Created by Khurshed Umarov on 05.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Main Screen"
+        view.backgroundColor = .white
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
